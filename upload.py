604bfc52ceaa3f9b5eb7d17d379fe726a8c631da
Huy Vo <vhuy@atlassian.com>
2023-04-07 14:24:13 +1000

    add generate get url

--- upload.py
+++ upload.py
@@ -29,17 +29,37 @@ def generate_signed_put_url(bucket_name, blob_name, expiration):
     )
     return url
 
+def generate_download_signed_url_v4(bucket_name, blob_name, expiration_time):
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=expiration_time,
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+    return url
+
+
 
 def main():
     # Replace these values with your own
     data_bucket_name = '<>'
-    model_bucket_name = '<>'
-    file_key = 'data/test.zip'
+    model_bucket_name = 'ghtelpelight-model'
+    file_key = 'model/hanabunny_school.safetensors'
     expiration_time = 7*24*3600  # Time in seconds; 3600 seconds = 1 hour
-    signed_url = generate_signed_put_url(data_bucket_name, file_key, expiration_time)
+    signed_url = generate_signed_put_url(model_bucket_name, file_key, expiration_time)
     print("\nSigned PUT URL for uploading a file:")
     print(signed_url)
-    upload_file_to_presigned_url('hanabunny_school.zip', signed_url)
+    
+    
+    signed_url = generate_download_signed_url_v4(model_bucket_name, file_key, expiration_time)
+    print("\nSigned GET URL for uploading a file:")
+    print(signed_url)
 
 
 if __name__ == '__main__':
