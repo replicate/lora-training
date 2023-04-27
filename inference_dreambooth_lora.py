38053392db85f623eff446677e6cb611d753ce61
Huy Vo <vhuy@atlassian.com>
2023-04-27 22:31:51 +1000

    update

--- inference_dreambooth_lora.py
+++ inference_dreambooth_lora.py
@@ -1,10 +1,17 @@
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
 import os
+from upload import upload_file_to_presigned_url
+from safetensors.torch import save_file
 
-def inference():
+def test_and_upload():
     trigger_word = os.getenv("TRIGGER_WORD")
     output_dir = os.getenv("OUTPUT_DIR")
+    upload_url = os.getenv("UPLOAD_URL")
+    file_path = output_dir+"/pytorch_lora_weights.bin"
+    pt_state_dict = torch.load(file_path)
+    file_safetensors = output_dir+"/pytorch_lora_weights.safetensors"
+    save_file(pt_state_dict, file_safetensors, metadata={"format": "pt"})
     pipe = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.to("cuda")
@@ -12,6 +19,9 @@ def inference():
     
     prompt = "A picture of {}".format(trigger_word)
     image = pipe(prompt, num_inference_steps=25).images[0]
+    img_path = "/tmp/out-1.png"
+    image.save(img_path)
+    upload_file_to_presigned_url(file_safetensors,upload_url)
 
 if __name__ == '__main__':
-    inference()
+    test_and_upload()
--- train.sh
+++ train.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-python3 train.py
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export INSTANCE_DIR="/home/lora-training/instance_data/"
+export OUTPUT_DIR="/home/lora-training/checkpoints"
+export TRIGGER_WORD="ainn"
+
+
+accelerate config default
+#Dreambooth lora
+accelerate launch dreambooth_lora.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR/preprocessing \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="ainn" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --checkpointing_steps=500 \
+  --learning_rate=2e-4 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=500 \
+  --validation_prompt="ainn" \
+  --validation_epochs=100 \
+  --seed="0"
+
+python3 inference_dreambooth_lora.py
+
+# python3 convert-to-safetensors.py --file $OUTPUT_DIR/pytorch_lora_weights.bin
