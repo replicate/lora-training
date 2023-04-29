22df8c030f5971d41c71abec8118cdd96c760cda
Huy Vo <vhuy@atlassian.com>
2023-04-29 14:02:27 +1000

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Trigger word: ${TRIGGER_WORD}"
+
 python3 file_manager.py
 
 rm -rf $INSTANCE_DIR/preprocessing/caption.txt
@@ -10,7 +12,7 @@ accelerate launch dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --instance_data_dir=$INSTANCE_DIR/preprocessing \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="ainn" \
+  --instance_prompt="${TRIGGER_WORD}" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
@@ -18,8 +20,9 @@ accelerate launch dreambooth_lora.py \
   --learning_rate=2e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
+  --report_to="wandb" \
   --max_train_steps=$STEP \
-  --validation_prompt="ainn" \
+  --validation_prompt="portrait photo of (${TRIGGER_WORD}), sharp focus, elegant, render, realistic skin texture, photorealistic, hyper realism, 4k, hdr, smooth" \
   --validation_epochs=100 \
   --seed="0"
 
